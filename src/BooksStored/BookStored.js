4905ea94f9d14bf07fdc9b371119d267d3d34ec5
abdullahjim29 <abdullahjim2002@gmail.com>
2025-01-23 17:47:07 +0600

    implement 'Wishlist' button and add to the localStorage

--- src/BooksStored/BookStored.js
+++ src/BooksStored/BookStored.js
@@ -11,17 +11,44 @@ const getStoredBooks = () => {
     }
 }
 
+
 const addToLocalStorage = id => {
     const storedBooks = getStoredBooks();
     if (storedBooks.includes(id)) {
-        toast.error('This Book is already added')
+        toast.error('This Book is already added to the read list')
     }
     else {
-        toast.success('Congrate! You Book is added')
+        toast.success('Congrate! Your Book is added to the read list')
         storedBooks.push(id);
         const storedBooksStr = JSON.stringify(storedBooks);
         localStorage.setItem('read-list', storedBooksStr);
     }
 }
 
-export {getStoredBooks, addToLocalStorage}
+
+const getStoredWishlistBooks = () => {
+    const storedWishlistBooks = localStorage.getItem('wish-list');
+    if(storedWishlistBooks){
+        const storedWishlist = JSON.parse(storedWishlistBooks);
+        return storedWishlist;
+    }
+    else{
+        return [];
+    }
+}
+
+
+const wishlistAddToLS = id => {
+    const storedWishlistBooks = getStoredWishlistBooks();
+    if(storedWishlistBooks.includes(id)){
+        toast.error('This Book is already added to the Wish list')
+    }
+    else{
+        toast.success('Congrate! Your Books is added to the Wish list')
+        storedWishlistBooks.push(id);
+        const wishlistStr = JSON.stringify(storedWishlistBooks);
+        localStorage.setItem('wish-list', wishlistStr)
+    }
+}
+
+export {getStoredBooks, addToLocalStorage , wishlistAddToLS, getStoredWishlistBooks }
--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,6 +1,11 @@
 import { useLoaderData, useParams } from "react-router-dom";
-import { addToLocalStorage } from "../../BooksStored/BookStored";
-import { Toaster } from "react-hot-toast";
+import {
+  addToLocalStorage,
+  getStoredBooks,
+  getStoredWishlistBooks,
+  wishlistAddToLS,
+} from "../../BooksStored/BookStored";
+import toast, { Toaster } from "react-hot-toast";
 
 const BookDetails = () => {
   const { bookId } = useParams();
@@ -23,38 +28,50 @@ const BookDetails = () => {
     yearOfPublishing,
   } = book;
 
-  const addToReadList = id => {
-    addToLocalStorage(id)
-  }
+  const addToReadList = (id) => {
+    addToLocalStorage(id);
+    // localStorage.removeItem('wish-list');
+  };
+
+  const handleWishList = (id) => {
+    const storedReadList = getStoredBooks();
+    if (storedReadList.includes(id)) {
+      toast.error("You have already read this Book");
+    } else {
+      wishlistAddToLS(id);
+    }
+  };
 
   return (
     <div className="my-10">
-        <Toaster/>
+      <Toaster />
       <div className="grid grid-col lg:grid-cols-2 gap-10">
         <div className="bg-base-200 p-20 rounded-2xl flex justify-center items-center">
-            <img className="h-[400px]" src={image} alt={bookName} />
+          <img className="h-[400px]" src={image} alt={bookName} />
         </div>
 
         <div className="space-y-4">
-            <h2 className="text-3xl font-bold">{bookName}</h2>
-            <p className="text-base">By : {author}</p>
-            <div className="divider"></div>
-            <p className="text-base">{category}</p>
-            <div className="divider"></div>
-            <p><span className="font-bold">Review</span> {review}</p>
-            <div className="flex items-center gap-8">
-                <p className="font-bold">Tag</p>
-                {
-                    tags.map((tag, idx) => <div
-                    key={idx}
-                    className="bg-base-200 px-5 py-2 rounded-full text-[#23BE0A] font-semibold"
-                  >
-                    #{tag}
-                  </div>)
-                }
-            </div>
-            <div className="divider"></div>
-            <div className="grid grid-cols-2 gap-y-2">
+          <h2 className="text-3xl font-bold">{bookName}</h2>
+          <p className="text-base">By : {author}</p>
+          <div className="divider"></div>
+          <p className="text-base">{category}</p>
+          <div className="divider"></div>
+          <p>
+            <span className="font-bold">Review</span> {review}
+          </p>
+          <div className="flex items-center gap-8">
+            <p className="font-bold">Tag</p>
+            {tags.map((tag, idx) => (
+              <div
+                key={idx}
+                className="bg-base-200 px-5 py-2 rounded-full text-[#23BE0A] font-semibold"
+              >
+                #{tag}
+              </div>
+            ))}
+          </div>
+          <div className="divider"></div>
+          <div className="grid grid-cols-2 gap-y-2">
             <p>Number of Pages:</p>
             <p className="font-bold">{totalPages}</p>
             <p>Publisher:</p>
@@ -63,9 +80,19 @@ const BookDetails = () => {
             <p className="font-bold">{yearOfPublishing}</p>
             <p>Rating:</p>
             <p className="font-bold">{rating}</p>
-            </div>
-            <button onClick={() => addToReadList (id)} className="btn font-bold text-black px-6 mr-6 bg-white border-black hover:bg-[#23BE0A] hover:text-white">Read</button>
-            <button className="btn font-bold px-6 bg-[#50B1C9] text-white hover:bg-[#23BE0A]">Wishlist</button>
+          </div>
+          <button
+            onClick={() => addToReadList(id)}
+            className="btn font-bold text-black px-6 mr-6 bg-white border-black hover:bg-[#23BE0A] hover:text-white"
+          >
+            Read
+          </button>
+          <button
+            onClick={() => handleWishList(id)}
+            className="btn font-bold px-6 bg-[#50B1C9] text-white hover:bg-[#23BE0A]"
+          >
+            Wishlist
+          </button>
         </div>
       </div>
     </div>
